9a03a932ac818a8b51150c578aa620a7d023ebb5
Phoebe Lau <phoebelau7@gmail.com>
2018-01-10 22:35:31 +0000

    automatically backed up by learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,10 +1,10 @@
 class Puppy
   attr_accessor :name, :breed, :age
 
-  def initialize(hash)
-    hash.each do |key, value|
-      self.send "#{key}=", value
-    end
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
   end
 
 end
